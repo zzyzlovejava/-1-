0f90e0a083bcf19359510590421bc974d5c4303a
zzyz <742367761@qq.com>
2024-07-03 11:06:10 +0800

    郑之杨子提交了全局配置文件和登录文件

---
+++ Login.aspx
@@ -0,0 +1,162 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>学分制教学管理系统</title>
+    <style type="text/css">
+        h1 {
+            margin-bottom: 20px;
+            color: #0080ff; /* 主色 */
+        }
+
+        .form-group {
+            margin-bottom: 15px;
+        }
+
+        .form-group label {
+            display: block;
+            margin-bottom: 5px;
+            font-weight: bold;
+        }
+
+        .form-group input[type="text"],
+        .form-group input[type="password"] {
+            padding: 8px;
+            border: 1px solid #ccc;
+        }
+
+        .btn-group {
+            margin-top: 20px;
+        }
+
+        .btn-group button {
+            padding: 10px 25px;
+            border: none;
+            background-color: #2ecc71; /* 辅助色1 */
+            color: #fff;
+            cursor: pointer;
+            text-align: center; /* 文字居中 */
+            font-size: 16px; /* 设置按钮字体大小 */
+        }
+
+        .btn-group button:hover {
+            background-color: #27ae60; /* 辅助色1 加深 */
+        }
+
+        .panel {
+            display: none;
+            margin-top: 20px;
+        }
+
+        .panel.active {
+            display: block;
+        }
+
+        .panel {
+            height: auto;
+            width: 300px; /* Or adjust according to your layout */
+            padding: 20px;
+            border: 1px solid #ccc;
+            background-color: #f9f9f9;
+            margin-bottom: 20px;
+        }
+
+        .label {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .links a {
+            text-decoration: none;
+            color: #0000ff;
+            margin-bottom : 56px;
+            padding: 8px 16px;
+            border: 1px solid #0000ff;
+        }
+
+        .links a:hover {
+            background-color: #0000ff;
+            color: #fff;
+        }
+    </style>
+</head>
+<body style="text-align: center;">
+    <form id="form1" runat="server">
+    <div style="text-align: center">
+        <h1>学生学习情况登记系统</h1>
+        <br />
+
+        <asp:Panel ID="LoginPanel" runat="server" Height="206px">
+            账号: <asp:TextBox ID="StuIDTextBox" runat="server" Width="150px" style="font-size:20px;  margin-bottom:10px"></asp:TextBox><br />
+            密码: <asp:TextBox ID="PassTextBox" runat="server" TextMode="Password" Width="150px" style="font-size:20px;"></asp:TextBox><br />
+            <br />
+            <div class="radio-group">
+                <asp:RadioButtonList ID="UserRoleRadio" runat="server" RepeatLayout="Flow" 
+                    RepeatDirection="Horizontal">
+                    <asp:ListItem Text="管理员" Value="Admin"></asp:ListItem>
+                    <asp:ListItem Text="教师" Value="Teacher"></asp:ListItem>
+                    <asp:ListItem Text="学生" Value="Student" Selected="True"></asp:ListItem>
+                </asp:RadioButtonList>
+            </div>
+            <br />
+            <asp:Button ID="LoginSubmitBtn" runat="server" CssClass="btn btn-fancy" Text="提交" OnClick="LoginSubmit_Click" Width="70px" />
+            &nbsp;
+            <asp:Button ID="LoginResetBtn" runat="server" CssClass="btn btn-fancy" Text ="重置" Width="70px" OnClick="LoginReset_Click" />
+        </asp:Panel>
+        <br />
+
+        <asp:Panel ID="LoginOkPanel" runat="server" Height="300px" 
+            Visible="False" HorizontalAlign="Center">
+            <asp:Label ID="LoginOKLabel" runat="server" Text="学生登录成功" CssClass="label" 
+                Font-Size="X-Large"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <div class="links">
+                <a href="student/SelectCourse.aspx">课程选修</a> <br /><br />
+                <a href="student/StuGradeQuery.aspx">成绩查询</a> <br /><br />
+                <a href="student/ReturnCourse.aspx">课程退选</a> <br /><br />
+                <a href="login.aspx?loginSuccess=false&LoginOkPanel=false&Panel1=false&Panel11=false">退出登录</a>
+            </div>
+            <br />
+        </asp:Panel>
+
+        <asp:Panel ID="Panel1" runat="server" Height="300px" Visible="False">
+            <asp:Label ID="Label1" runat="server" Text="教师登录成功" CssClass="label" 
+                Font-Size="X-Large" ></asp:Label>
+            <br />
+            <br />
+            <br />
+            <div class="links">
+                <a href="teacher/GradeProcess.aspx">成绩录入</a> <br /> <br /> 
+                <a href="teacher/TeacherGradeQuery.aspx">成绩查询</a> <br /><br /> 
+                <a href="teacher/StuGradeQuery.aspx">平均成绩查询</a> <br /><br /> 
+                <a href="login.aspx?loginSuccess=false&LoginOkPanel=false&Panel1=false&Panel11=false">退出登录</a> <br />
+            </div>
+        </asp:Panel>
+
+        <asp:Panel ID="Panel11" runat="server"  Height="300px" Visible="False">
+            <asp:Label ID="Label11" runat="server" Text="管理员登录成功"   CssClass="label" 
+                Font-Size="X-Large"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <div class="links">
+                <a href="admin/ClassAdd.aspx">新增班级</a> <br />  <br />
+                <a href="admin/ClassManage.aspx">班级管理</a> <br /><br />
+                <a href="admin/ClassStuGradeQuery.aspx">班级成绩查询</a> <br /><br />
+                <a href="admin/TeacherGradeQuery.aspx">教师课程班成绩查询</a> <br /><br />
+                <a href="admin/StuAdd.aspx">新增学生</a> <br /> <br />
+                <a href="admin/StuManage.aspx">学生管理</a> <br /><br /> 
+                <a href="login.aspx?loginSuccess=false&LoginOkPanel=false&Panel1=false&Panel11=false">退出登录</a>
+            </div>                
+            <br />
+        </asp:Panel>
+            <br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string loginSuccess = Request.QueryString["loginSuccess"];
+            if (loginSuccess != null && loginSuccess.Equals("true"))
+            {
+                string code = Session["code"].ToString();
+                this.LoginPanel.Visible = false;   //隐藏登录区域
+                if (code.Equals("1"))
+                {
+                    // 如果LoginOkPanel的可见性的标识存在且为true，则将Panel1的Visible属性设置为true
+                    this.LoginOkPanel.Visible = true;
+                    LoginOKLabel.Text = Session["name"].ToString();
+                }
+                if (code.Equals("2"))
+                {
+                    // 如果Panel1的可见性的标识存在且为true，则将Panel1的Visible属性设置为true
+                    this.Panel1.Visible = true;
+                    Label1.Text = Session["name"].ToString();
+                }
+                if (code.Equals("3"))
+                {
+                    // 如果Panel11的可见性的标识存在且为true，则将Panel1的Visible属性设置为true
+                    this.Panel11.Visible = true;
+                    Label11.Text = Session["name"].ToString();
+                }
+            }
+        }
+    }
+
+    // 处理重置按钮点击事件的代码
+    protected void LoginReset_Click(object sender, EventArgs e)
+    {
+        StuIDTextBox.Text = "";
+        PassTextBox.Text = "";
+    }
+
+    // 处理登录按钮点击事件的代码
+    protected void LoginSubmit_Click(object sender, EventArgs e)
+    {
+        Session["code"] = 0;
+        string selectedRole = UserRoleRadio.SelectedValue;
+        if (string.IsNullOrEmpty(StuIDTextBox.Text) || string.IsNullOrEmpty(PassTextBox.Text))
+        {
+            Response.Write("<script language='javascript'>alert('账号或密码不能为空！！！');</script>");
+        }
+        else
+        {
+            // 执行管理员登录逻辑
+            if (selectedRole == "Admin")
+            {
+                string TeacherID = this.StuIDTextBox.Text;        //用户名 
+                string TeacherPassword = this.PassTextBox.Text;   //密码框
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+                SqlConnection TeachingWebConn = new SqlConnection(connectionString);
+                TeachingWebConn.Open();         //数据库连接打开
+                SqlCommand LoginCmd = new SqlCommand("SELECT * FROM TB_Teacher WHERE TeacherID='" + TeacherID + "' AND TPassword='" + TeacherPassword + "'", TeachingWebConn);
+                SqlDataReader RsLogin = LoginCmd.ExecuteReader();
+                RsLogin.Read();
+                if (RsLogin.HasRows)
+                {
+                    // 登录成功后将教师ID存储到 Session 中
+                    Session["TeacherID"] = TeacherID; // 替换为实际的教师ID
+                    Label11.Text = RsLogin["TeacherName"].ToString() + ",欢迎您！";
+                    Session["name"] = Label11.Text;
+                    Session["code"] = 3;
+                    this.LoginPanel.Visible = false;   //隐藏登录区域
+                    this.Panel11.Visible = true;    //显示登录成功区域
+                }
+                else
+                {
+                    //学号或密码错误处理代码；
+                    Response.Write("<SCRIPT language='javascript'>alert('账号或密码错误！！！'); </SCRIPT>");
+                }
+                RsLogin.Close();                         //登录记录集关闭
+                TeachingWebConn.Close();       //数据库连接关闭
+            }
+            // 执行教师登录逻辑
+            else if (selectedRole == "Teacher")
+            {
+                string TeacherID = this.StuIDTextBox.Text;        //用户名 
+                string TeacherPassword = this.PassTextBox.Text;   //密码框
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+                SqlConnection TeachingWebConn = new SqlConnection(connectionString);
+                TeachingWebConn.Open();         //数据库连接打开
+                SqlCommand LoginCmd = new SqlCommand("SELECT * FROM TB_Teacher WHERE TeacherID='" + TeacherID + "' AND TPassword='" + TeacherPassword + "'", TeachingWebConn);
+                SqlDataReader RsLogin = LoginCmd.ExecuteReader();
+                RsLogin.Read();
+                if (RsLogin.HasRows)
+                {
+                    // 登录成功后将教师ID存储到 Session 中
+                    Session["TeacherID"] = TeacherID; // 替换为实际的教师ID
+                    Label1.Text = RsLogin["TeacherName"].ToString() + ",欢迎您！";
+                    Session["name"] = Label1.Text;
+                    Session["TeacherName"] = RsLogin["TeacherName"];
+                    Session["code"] = 2;
+                    this.LoginPanel.Visible = false;   //隐藏登录区域
+                    this.Panel1.Visible = true;    //显示登录成功区域
+                }
+                else
+                {
+                    //学号或密码错误处理代码；
+                    Response.Write("<SCRIPT language='javascript'>alert('账号或密码错误！！！'); </SCRIPT>");
+                }
+                RsLogin.Close();                         //登录记录集关闭
+                TeachingWebConn.Close();       //数据库连接关闭
+            }
+            // 执行学生登录逻辑
+            else if (selectedRole == "Student")
+            {
+                string StuID = StuIDTextBox.Text;
+                string StuPassword = PassTextBox.Text;
+                SqlConnection TeachingWebConn = new SqlConnection("server=127.0.0.1;uid=sa;pwd=123456;database=DB_TeachingMS");
+                TeachingWebConn.Open();
+                SqlCommand LoginCmd = new SqlCommand("SELECT * FROM TB_Student WHERE StuID=@StuID AND SPassword=@StuPassword", TeachingWebConn);
+                LoginCmd.Parameters.AddWithValue("@StuID", StuID);
+                LoginCmd.Parameters.AddWithValue("@StuPassword", StuPassword);
+                SqlDataReader RsLogin = LoginCmd.ExecuteReader();
+                if (RsLogin.Read())
+                {
+                    // 登录成功，将学生学号存储到 Session 中
+                    Session["StuID"] = StuID;
+                    LoginOKLabel.Text = RsLogin["StuName"].ToString() + ",欢迎您！";
+                    Session["name"] = LoginOKLabel.Text;
+                    Session["StuName"] = RsLogin["StuName"];
+                    Session["code"] = 1;
+                    LoginPanel.Visible = false;
+                    LoginOkPanel.Visible = true;
+                }
+                else
+                {
+                    Response.Write("<SCRIPT language='javascript'>alert('学号或密码错误！！！'); </SCRIPT>");
+                }
+                RsLogin.Close();
+                TeachingWebConn.Close();
+            }
+            else
+            {
+                // 默认情况下的处理逻辑
+
+            }
+        }
+    }
+
+}
